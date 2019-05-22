cd47fd884700c4b00a74a88b70749f5e091d574c
Kimserey <kimserey.lam@gmail.com>
2019-05-22 21:27:48 +0100

    Separated amb evaluator from example
    Augmented all analyzer functions with succeed and fail continuations

---
+++ 4-3-1-non-deterministic-computing-amb-example.rkt
@@ -0,0 +1,100 @@
+#lang racket
+
+
+; ***********************************************************************************************
+(define (require p)
+  (if (not p) (amb) #f))
+
+(define (an-element-of items)
+  (require (not (null? items)))
+  (amb (car items)
+       (an-element-of (cdr items))))
+
+; Baker, Cooper, Fletcher, Miller, and Smith live on different floors of an apartment house
+; that contains only five floors.
+; * Baker does not live on the top floor.
+; * Cooper does not live on the bottom floor.
+; * Fletcher does not live on either the top or the bottom floor.
+; * Miller lives on a higher floor than does Cooper.
+; * Smith does not live on a floor adjacent to Fletcher’s.
+; * Fletcher does not live on a floor adjacent to Cooper’s.
+; Where does everyone live?
+(define (multiple-dwelling)
+  (let ([baker (amb 1 2 3 4 5)]
+        [cooper (amb 1 2 3 4 5)]
+        [fletcher (amb 1 2 3 4 5)]
+        [miller (amb 1 2 3 4 5)]
+        [smith (amb 1 2 3 4 5)])
+    (require (distinct? (list baker cooper fletcher miller smith)))
+    (require (not (= baker 5)))
+    (require (not (= cooper 1)))
+    (require (not (= fletcher 5)))
+    (require (not (= fletcher 1)))
+    (require (> miller cooper))
+    (require (not (= (abs (- smith flether)) 1)))
+    (require (not (= (abs (- fletcher cooper)) 1)))
+
+    (list (cons 'baker baker)
+          (cons 'cooper cooper)
+          (cons 'fletcher fletcher)
+          (cons 'miller miller)
+          (cons 'smith smith))))
+
+; Parsing natural language
+
+(define nouns
+  '(noun student professor cat class))
+
+(define verbs
+  '(verb studies lectures eats sleeps))
+
+(define articles
+  '(article the a))
+
+(define prepositions
+  '(prep for to in by with))
+   
+(define (parse-prepositional-phrase)
+  (list 'prep-phrase
+        (parse-word prepositions)
+        (parse-noun-phrase)))
+
+; The grammar defines that a sentence is composed by a noun phrase, followed by a verb.
+; A noun phrase is composed of an article followed by a noun.
+; "The cat east" is parsed as "(sentence (noun-phrase (article the) (noun cat)) (verb eats))"
+(define (parse-sentence)
+  (list 'sentence
+        (parse-noun-phrase)
+        (parse-verb-phrase)))
+
+(define (parse-simple-noun-phrase)
+  (list 'simple-noun-phrase
+        (parse-word articles)
+        (parse-word nouns)))
+
+(define (parse-noun-phrase)
+  (define (maybe-extend noun-phrase)
+    (amb noun-phrase (maybe-extend (list 'noun-phrase noun-phrase (parse-prepositional-phrase)))))
+  (maybe-extend (parse-simple-noun-phrase)))
+
+(define (parse-verb-phrase)
+  (define (maybe-extend verb-phrase)
+    (amb verb-phrase (maybe-extend (list 'verb-phrase verb-phrase (parse-prepositional-phrase)))))
+  (maybe-extend (parse-word verbs)))
+
+(define (parse-word word-list)
+  (require (not (null? *unparsed*)))
+  (require (memq (car *unparsed*) ; memq: take in list from member equal to first argument.
+                 (cdr word-list)))
+  (let ([found-word (car *unparsed*)])
+    (set! *unparsed* (cdr *unparsed*))
+    (list (car word-list) found-word)))
+
+(define *unparsed* '()) ; global variable holding all unparsed words
+
+(define (parse input)
+  (set! *unparsed input)
+  (let ([sent (parse-sentence)])
+    (require (null? *unparsed*))
+    sent))
+
--- 4-3-1-non-deterministic-computing.rkt
+++ 4-3-1-non-deterministic-computing.rkt
@@ -32,48 +32,64 @@
         [else
          (error "Unknown expression type: ANALYZE" exp)]))
 
-(define (analyze-self-evaluating exp) (lambda (env) exp))
+(define (analyze-self-evaluating exp) (lambda (env succeed fail) (succeed exp fail)))
 
 (define (analyze-quoted exp)
   (let ([qval (text-of-quotation exp)])
-    (lambda (env) qval)))
+    (lambda (env succeed fail) (succeed qval fail))))
 
 (define (analyze-variable exp)
-  (lambda (env)
-    (lookup-variable-value exp env)))
+  (lambda (env succeed fail)
+    (succeed (lookup-variable-value exp env)
+             fail)))
 
 (define (analyze-assignment exp)
   (let ([var (assignment-variable exp)]
         [vproc (analyze (assignment-value exp))])
-  (lambda (env)
-    (set-variable-value! var (vproc env) env)
-    'ok)))
+    (lambda (env succeed fail)
+      (vproc env
+             (lambda (val fail2)
+               (let ([old-value (lookup-variable-value var env)])
+                 (set-variable-value! var val env)
+                 (succeed 'ok (lambda () (set-variable-value! var old-value env) (fail2)))))
+             fail))))
 
 (define (analyze-definition exp)
   (let ([var (definition-variable exp)]
         [vproc (analyze (definition-value exp))])
-   (lambda (env)
-    (define-variable! var (vproc env) env)
-    'ok)))
+   (lambda (env succeed fail)
+    (vproc env
+           (lambda (val fail2)
+             (define-variable! var val env)
+             (succeed 'ok fail2))
+           fail))))
 
 (define (analyze-if exp)
   (let ([pproc (analyze (if-predicate exp))]
         [cproc (analyze (if-consequent exp))]
         [aproc (analyze (if-alternative exp))])
-  (lambda (env)
-    (if (true? (pproc env))
-      (cproc env)
-      (aproc env)))))
+  (lambda (env succeed fail)
+    (pproc env
+           (lambda (pred-value fail2)
+             (if (true? pred-value)
+                 (cproc env succeed fail2)
+                 (aproc env succeed fail2)))
+           fail))))
 
 (define (analyze-lambda exp)
   (let ([vars (lambda-parameters exp)]
         [bproc (lambda-body exp)])
-    (lambda (env)
-      (make-procedure vars bproc env))))
+    (lambda (env succeed fail)
+      (succeed (make-procedure vars bproc env)
+               fail))))
 
 (define (analyze-sequence exp)
-  (define (sequentially proc1 proc2)
-    (lambda (env) (proc1 env) (proc2 env)))
+  (define (sequentially a b)
+    (lambda (env succeed fail)
+      (a env
+         (lambda (a-value fail2)
+           (b env succeed fail2))
+         fail)))
   (define (loop first-proc rest-procs)
     (if (null? rest-procs)
         first-proc
@@ -87,18 +103,41 @@
 (define (analyze-application exp)
   (let ([fproc (analyze (operator exp))]
         [aprocs (map analyze (operands exp))])
-    (lambda (env)
-      (execute-application (fproc env) (map (lambda (aproc) (aproc env)) aprocs)))))
+    (lambda (env succeed fail)
+      (fproc env
+             (lambda (proc fail2)
+               (get-args aprocs
+                         env
+                         (lambda (args fail3) (execute-application proc args succeed fail3))
+                         fail2))
+             fail))))
 
-(define (execute-application proc args)
+(define (get-args aprocs env succeed fail)
+  (if (null? aprocs)
+      (succeed '() fail)
+      ((car aprocs)
+       env
+       (lambda (arg fail2)
+         (get-args
+          (cdr aprocs)
+          env
+          (lambda (args fail3)
+            (succeed (cons arg args)
+                     fail3))
+          fail2))
+       fail)))
+
+(define (execute-application proc args succeed fail)
   (cond [(primitive-procedure? proc)
-         (apply-primitive-procedure proc args)]
+         (succeed (apply-primitive-procedure proc args) fail)]
         [(compound-procedure? proc)
          ((analyze-sequence (procedure-body proc))
           (extend-environment
            (procedure-parameters proc)
            args
-           (procedure-environment proc)))]
+           (procedure-environment proc))
+          succeed
+          fail)]
         [else (error "Unknown procedure type: EXECUTE-APPLICATION" proc)]))
 
 (define (self-evaluating? exp)
@@ -346,17 +385,37 @@
 
 (define the-global-environment (setup-environment))
 
-(define input-prompt ";;; M-Eval input:")
+(define input-prompt ";;; Amb-Eval input:")
 
-(define output-prompt ";;; M-Eval value:")
+(define output-prompt ";;; Amb-Eval value:")
 
 (define (driver-loop)
-  (prompt-for-input input-prompt)
-  (let ([input (read)])
-    (let ([output (eval input the-global-environment)])
-      (announce-output output-prompt)
-      (user-print output)))
-  (driver-loop))
+  (define (internal-loop try-again)
+    (prompt-for-input input-prompt)
+    (let ([input (read)])
+      (if (eq? input 'try-again)
+          (try-again)
+          (begin
+            (newline)
+            (display
+             ";;; Starting new problem")
+            (ambeval
+             input
+             the-global-environment
+             (lambda (val next-alternative)
+               (announce-output output-prompt)
+               (user-print val)
+               (internal-loop next-alternative))
+             (lambda ()
+               (announce-output ";;; There are no more values of")
+               (user-print input)
+               (driver-loop)))))))
+  (internal-loop
+   (lambda ()
+     (newline)
+     (display
+      ";;; There is no current problem")
+     (driver-loop))))
 
 (define (prompt-for-input string)
   (newline)
@@ -378,9 +437,6 @@
              '<procedure-env>))
       (display object)))
 
-;; Run the evaluator:
-; (driver-loop)
-
 ; ***********************************************************************************************
 ; amb
 
@@ -391,100 +447,17 @@
 (define (ambeval exp env succeed fail)
   ((analyze exp) env succeed fail))
 
-; ***********************************************************************************************
-(define (require p)
-  (if (not p) (amb)))
+(define (analyze-amb exp)
+  (let ([cprocs (map analyze (amb-choices exp))])
+    (lambda (env succeed fail)
+      (define (try-next choices)
+        (if (null? choices)
+            (fail)
+            ((car choices)
+             env
+             succeed
+             (lambda ()
+               (try-next (cdr choices))))))
+      (try-next cprocs))))
 
-(define (an-element-of items)
-  (require (not (null? items)))
-  (amb (car items)
-       (an-element-of (cdr items))))
-
-; Baker, Cooper, Fletcher, Miller, and Smith live on different floors of an apartment house
-; that contains only five floors.
-; * Baker does not live on the top floor.
-; * Cooper does not live on the bottom floor.
-; * Fletcher does not live on either the top or the bottom floor.
-; * Miller lives on a higher floor than does Cooper.
-; * Smith does not live on a floor adjacent to Fletcher’s.
-; * Fletcher does not live on a floor adjacent to Cooper’s.
-; Where does everyone live?
-(define (multiple-dwelling)
-  (let ([baker (amb 1 2 3 4 5)]
-        [cooper (amb 1 2 3 4 5)]
-        [fletcher (amb 1 2 3 4 5)]
-        [miller (amb 1 2 3 4 5)]
-        [smith (amb 1 2 3 4 5)])
-    (require (distinct? (list baker cooper fletcher miller smith)))
-    (require (not (= baker 5)))
-    (require (not (= cooper 1)))
-    (require (not (= fletcher 5)))
-    (require (not (= fletcher 1)))
-    (require (> miller cooper))
-    (require (not (= (abs (- smith flether)) 1)))
-    (require (not (= (abs (- fletcher cooper)) 1)))
-
-    (list (cons 'baker baker)
-          (cons 'cooper cooper)
-          (cons 'fletcher fletcher)
-          (cons 'miller miller)
-          (cons 'smith smith))))
-
-; Parsing natural language
-
-(define nouns
-  '(noun student professor cat class))
-
-(define verbs
-  '(verb studies lectures eats sleeps))
-
-(define articles
-  '(article the a))
-
-(define prepositions
-  '(prep for to in by with))
-   
-(define (parse-prepositional-phrase)
-  (list 'prep-phrase
-        (parse-word prepositions)
-        (parse-noun-phrase)))
-
-; The grammar defines that a sentence is composed by a noun phrase, followed by a verb.
-; A noun phrase is composed of an article followed by a noun.
-; "The cat east" is parsed as "(sentence (noun-phrase (article the) (noun cat)) (verb eats))"
-(define (parse-sentence)
-  (list 'sentence
-        (parse-noun-phrase)
-        (parse-verb-phrase)))
-
-(define (parse-simple-noun-phrase)
-  (list 'simple-noun-phrase
-        (parse-word articles)
-        (parse-word nouns)))
-
-(define (parse-noun-phrase)
-  (define (maybe-extend noun-phrase)
-    (amb noun-phrase (maybe-extend (list 'noun-phrase noun-phrase (parse-prepositional-phrase)))))
-  (maybe-extend (parse-simple-noun-phrase)))
-
-(define (parse-verb-phrase)
-  (define (maybe-extend verb-phrase)
-    (amb verb-phrase (maybe-extend (list 'verb-phrase verb-phrase (parse-prepositional-phrase)))))
-  (maybe-extend (parse-word verbs)))
-
-(define (parse-word word-list)
-  (require (not (null? *unparsed*)))
-  (require (memq (car *unparsed*) ; memq: take in list from member equal to first argument.
-                 (cdr word-list)))
-  (let ([found-word (car *unparsed*)])
-    (set! *unparsed* (cdr *unparsed*))
-    (list (car word-list) found-word)))
-
-(define *unparsed* '()) ; global variable holding all unparsed words
-
-(define (parse input)
-  (set! *unparsed input)
-  (let ([sent (parse-sentence)])
-    (require (null? *unparsed*))
-    sent))
-
+(driver-loop)
