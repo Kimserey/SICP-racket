8421539b12ceea05db404200cecd9297574cbadd
Kimserey <kimserey.lam@gmail.com>
2019-05-22 18:20:59 +0100

    Added analysis execute changes

--- 4-3-1-non-deterministic-computing.rkt
+++ 4-3-1-non-deterministic-computing.rkt
@@ -38,9 +38,7 @@
           (cons 'miller miller)
           (cons 'smith smith))))
 
-;
 ; Parsing natural language
-;
 
 (define nouns
   '(noun student professor cat class))
@@ -51,18 +49,36 @@
 (define articles
   '(article the a))
 
+(define prepositions
+  '(prep for to in by with))
+   
+(define (parse-prepositional-phrase)
+  (list 'prep-phrase
+        (parse-word prepositions)
+        (parse-noun-phrase)))
+
 ; The grammar defines that a sentence is composed by a noun phrase, followed by a verb.
 ; A noun phrase is composed of an article followed by a noun.
 ; "The cat east" is parsed as "(sentence (noun-phrase (article the) (noun cat)) (verb eats))"
 (define (parse-sentence)
   (list 'sentence
         (parse-noun-phrase)
-        (parse-word verbs)))
+        (parse-verb-phrase)))
 
-(define (parse-noun-phrase)
-  (list 'noun-phrase
+(define (parse-simple-noun-phrase)
+  (list 'simple-noun-phrase
         (parse-word articles)
         (parse-word nouns)))
+
+(define (parse-noun-phrase)
+  (define (maybe-extend noun-phrase)
+    (amb noun-phrase (maybe-extend (list 'noun-phrase noun-phrase (parse-prepositional-phrase)))))
+  (maybe-extend (parse-simple-noun-phrase)))
+
+(define (parse-verb-phrase)
+  (define (maybe-extend verb-phrase)
+    (amb verb-phrase (maybe-extend (list 'verb-phrase verb-phrase (parse-prepositional-phrase)))))
+  (maybe-extend (parse-word verbs)))
 
 (define (parse-word word-list)
   (require (not (null? *unparsed*)))
@@ -71,3 +87,12 @@
   (let ([found-word (car *unparsed*)])
     (set! *unparsed* (cdr *unparsed*))
     (list (car word-list) found-word)))
+
+(define *unparsed* '()) ; global variable holding all unparsed words
+
+(define (parse input)
+  (set! *unparsed input)
+  (let ([sent (parse-sentence)])
+    (require (null? *unparsed*))
+    sent))
+
